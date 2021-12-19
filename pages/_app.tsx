8ded20fc5f4952bc6925aa2bc5ead182f6533aa0
Robson Natanael <natanaelrobson@gmail.com>
2021-12-19 11:24:37 -0300

    feat(style): create style global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import GlobalStyles from 'src/styles/global';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default App
+export default App;
